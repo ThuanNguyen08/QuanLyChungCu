bb3a700ae5b8f588cb8e549ff04e3431feff72f7
ThuanNguyen08 <cntt44dnguyenvanthuan0990@gmail.com>
2024-06-14 13:43:22 +0700

    update _layoutKhach

---
+++ QuanLyChungCu/QuanLyChungCu/Controllers/KhachController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyChungCu.Controllers
+{
+    public class KhachController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
